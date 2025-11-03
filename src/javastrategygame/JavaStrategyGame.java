915486b9130fe06d4a87507884845f0b9e1984b0
ARombakh <artromb@mail.ru>
2025-11-03 13:16:51 +0300

    Screen and ability to draw rectangles of cells added

--- src/javastrategygame/JavaStrategyGame.java
+++ src/javastrategygame/JavaStrategyGame.java
@@ -10,18 +10,16 @@ package javastrategygame;
  */
 public class JavaStrategyGame {
 
-    final private static int FLD_WIDTH = 10;
-    final private static int FLD_HEIGHT = 10;
+    final private static int FLD_WIDTH = 3;
+    final private static int FLD_HEIGHT = 3;
     final private static int CELL_WIDTH = 5;
     final private static int CELL_HEIGHT = 4;
     
-    public static char[][] screen;
-    
-    class Unit{
+    static class Unit{
         public int Player;
         public int Type;
     }
-    class Building{}
+    static class Building{}
     
     // почему нужно обязательно указывать static?
     static class GameCell {
@@ -30,12 +28,26 @@ public class JavaStrategyGame {
         public Unit unit;
         public Building building;
         
-        public char[][] cellChars;
+        public char[][] cellChars = new char[CELL_WIDTH][CELL_HEIGHT];
+        
+        // Нужно ли инициализировать ячейку?
+        public void initCell() {
+            int x, y;
+            
+            for (x = 0; x < CELL_WIDTH; x++) {
+                for (y = 0; y < CELL_HEIGHT; y++) {
+                    cellChars[x][y] = ' ';
+                }
+            }
+        }
         
         public GameCell(int terrainType) {
             this.terrainType = terrainType;
             this.unit = null;
-            this.building = null;
+            this.building = new Building();
+            // в чём проблема в overridable методе в конструкторе?
+            // Почему выскакивает предупреждение?
+            initCell();
         }
         
         public String TerrainName() {
@@ -48,14 +60,62 @@ public class JavaStrategyGame {
             }
             return terrName;
         }
+
+        
+        public void fillBorder(boolean thick) {
+            int x, y, i;
+            
+            int[] bordX = {0, CELL_WIDTH - 1};
+            int[] bordY = {0, CELL_HEIGHT - 1};
+            
+            cellChars[bordX[0]][bordY[0]] = thick ? (char) 0x250F :
+                                                    (char) 0x250C;
+            cellChars[bordX[1]][bordY[0]] = thick ? (char) 0x2513 :
+                                                    (char) 0x2510;
+            cellChars[bordX[0]][bordY[1]] = thick ? (char) 0x2517 :
+                                                    (char) 0x2514;
+            cellChars[bordX[1]][bordY[1]] = thick ? (char) 0x251B :
+                                                    (char) 0x2518;
+            for (y = 1; y < bordY[1]; y++) {
+                for (i = 0; i < bordX.length; i++) {
+                    x = bordX[i];
+                    cellChars[x][y] = thick ? (char) 0x2503 : (char) 0x2502;
+                }
+            }
+            
+            for (x = 1; x < bordX[1]; x++) {
+                for (i = 0; i < bordY.length; i++) {
+                    y = bordY[i];
+                    cellChars[x][y] = thick ? (char) 0x2501 : (char) 0x2500;
+                }
+            }
+        }
         
         public void fillCellChars() {
-            
+            int x, y;
+            if (this.building != null) {
+                fillBorder(true);
+            }
+            else {
+                if (this.unit != null) {
+                    fillBorder(false);
+                }
+            }
+        }
+        
+        public void printCell() {
+            int x, y;
+            for (y = 0; y < CELL_HEIGHT; y++) {
+                for (x = 0; x < CELL_WIDTH; x++) {
+                    System.out.print(this.cellChars[x][y]);
+                }
+                System.out.println("");
+            }
         }
     }
 
     // почему нужно обязательно указывать static?    
-    static class Field {
+    public static class Field {
         public int width;
         public int height;
         public GameCell[][] cells;
@@ -89,12 +149,67 @@ public class JavaStrategyGame {
         }
     }
     
+    static class Screen {
+        public char[][] screen = new char[FLD_WIDTH * CELL_WIDTH]
+                                        [FLD_HEIGHT * CELL_HEIGHT];
+        public Field field;
+
+        public Screen() {
+            int x, y;
+
+            for (y = 0; y < screen[0].length; y++) {
+                for (x = 0; x < screen.length; x++) {
+                    this.screen[x][y] = ' ';
+                }
+            }
+        }
+        
+        public void assignCell(GameCell Cell, int cellX, int cellY) {
+            int strtX = cellX * CELL_WIDTH;
+            int strtY = cellY * CELL_HEIGHT; // Offset coordinates in the screen
+            int x, y; // Coordinates to loop in the screen
+
+            for (y = 0; y < CELL_HEIGHT; y++) {
+                for (x = 0; x < CELL_WIDTH; x++) {
+                    this.screen[x + strtX][y + strtY] = Cell.cellChars[x][y];
+                }
+            }
+        }
+        
+        public void assignAllCells(Field field) {
+            int x, y;
+            
+            for (y = 0; y < FLD_HEIGHT; y++) {
+                for (x = 0; x < FLD_WIDTH; x++) {
+                    assignCell(field.cells[x][y], x, y);
+                }                
+            }
+        }
+        
+        public void printScreen() {
+            int x, y;
+            
+            for (y = 0; y < FLD_HEIGHT * CELL_HEIGHT; y++) {
+                for (x = 0; x < FLD_WIDTH * CELL_WIDTH; x++) {
+                    System.out.print(screen[x][y]);
+                }
+                System.out.println("");
+            }
+        }
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         Field field = new Field(10, 10);
         field.DrawField();
-        System.out.println(field.cells[0][0].TerrainName());
+                
+        field.cells[0][0].fillBorder(true);
+        field.cells[1][1].fillBorder(false);
+        
+        Screen screen = new Screen();
+        screen.assignAllCells(field);
+        screen.printScreen();
     }   
 }
