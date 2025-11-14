19834da75e2606adfee6a905e50321e1a9d863d7
ARombakh <artromb@mail.ru>
2025-11-14 19:19:14 +0300

    Scanner was taken out of turn class

--- src/strategygame/Game.java
+++ src/strategygame/Game.java
@@ -16,6 +16,7 @@ public class Game {
     StrategyGame.Field.Legend legend;
     StrategyGame.Field.Screen screen;
     StrategyGame.Field.Player playerTurn;
+    Scanner scanner = new Scanner(System.in);
 
     public Game() {
         this.field = initField();
@@ -65,13 +66,12 @@ public class Game {
         boolean action;
         StrategyGame.Direction dir = null;
         StrategyGame.Field.Unit unitToGo = null;
-        Scanner scanner = new Scanner(System.in);
 
         unitToGo = field.Player[i].unit;
 
         System.out.printf("Turn of Player %c.\n", field.Player[i].symbol);
         System.out.print("Will it be action [y/n]? ");
-        yes_no = scanner.next();
+        yes_no = this.scanner.next();
 
         action = yes_no.toLowerCase().equals("y");
 
@@ -83,13 +83,13 @@ public class Game {
         }
 
         System.out.print("Enter direction: ");
-        move = scanner.next();
+        move = this.scanner.next();
 
         while ((dir = getDir(move)) == null) {
             System.out.print(
                     "Incorrect direction. Enter another direction: "
             );
-            move = scanner.next();
+            move = this.scanner.next();
         }
 
         if (action) {
